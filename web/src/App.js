6af093e0542c5d932426693d6a3872bb13b62563
Joshua Smith <jwsmaker@Joshuas-MacBook-Air.local>
2020-01-18 13:12:52 -0700

    feature-added: navbar and routing: added bootstrap and styled-components library

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './styles/App.css';
+import Routes from './Routes'
+import {Container, Row, Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+// import style from 'styled-components'
 
 function App() {
   return (
     <div className="App">
-      <h1>JobScraper Splash</h1>
+      
+      <>
+  <Container> 
+  
+  <Navbar bg="dark" variant="dark">
+    
+  </Navbar>
+  <Navbar bg="light" variant="light">
+    <Nav.Link href="#home">
+    <Navbar.Brand href="#home">
+      <img
+        alt="JobScraper Logo"
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{'    '}
+      Job Scraper
+    </Navbar.Brand>
+    </Nav.Link>
+    <Nav className="mr-auto">
+      <Nav.Link href="/front-end">Front End</Nav.Link>
+      <Nav.Link href="/back-end">Back End</Nav.Link>
+      <Nav.Link href="/dev-ops">Dev-Ops</Nav.Link>
+      <Nav.Link href="/qa">QA</Nav.Link>
+    </Nav>
+    {/* <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-md-2" />
+      <Button variant="outline-primary">Search</Button>
+    </Form> */}
+  </Navbar>
+  </Container>
+</>
+    <div className="screen">
+    <Routes/>
+    </div>
     </div>
   );
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
+import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
