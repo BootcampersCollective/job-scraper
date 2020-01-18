9d8987fb289056b4886c3a8b1d0fcd326d77e438
Joshua Smith <jwsmaker@Joshuas-MacBook-Air.local>
2020-01-18 08:32:35 -0700

    basic files structure setup for app

---
+++ web/job-scraper-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JobScraper Splash</h1>
+    </div>
+  );
+}
+
+export default App;
