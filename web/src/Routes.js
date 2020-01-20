cf265f891a86d99ef69f1fee4363596236075b06
Joshua W Smith <ws.joshua@gmail.com>
2020-01-19 19:01:06 -0700

    Web: bootstrap nav and library (#2)
    Web: navbar and routing: added bootstrap and styled-components library

---
+++ web/src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import Test from './screens/test'
+
+const Sample = ()=>{
+  return <h1>Sample Functional Component</h1>
+}
+
+export default () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" exact component={ Test } />
+      <Route exact path="/front-end" exact component={ Sample } />
+      <Route exact path="/back-end" exact component={ Sample } />
+      <Route exact path="/qa" exact component={ Sample } />
+    </Switch>
+  </Router>
+)
---
+++ web/src/screens/test.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+import React from 'react'
+// import { TopHomeContainer, AboutUs, AdoptableDogs, Partners, ContactUs } from '../components'
+
+const Test = () => (
+  <div>
+    <h1>Test Component Rendered By React-router-dom</h1>
+  </div>
+)
+
+export default Test
