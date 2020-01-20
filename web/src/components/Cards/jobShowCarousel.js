e89db9bbde86f31d0fa77a20964ddb3c326ea60d
Joshua Smith <jwsmaker@Joshuas-MacBook-Air.local>
2020-01-20 12:42:41 -0700

    formatted showCard

--- web/src/components/Cards/jobShowCarousel.js
+++ web/src/components/Cards/jobShowCarousel.js
@@ -1,32 +1,33 @@
 import React from 'react'
 import  {Carousel,Card} from 'react-bootstrap'
 
-const items = (source) => {
-    console.log(source)
-    return source.map((image)=> {
+const items = (imgArr) => {
+    
+    return imgArr.map((image)=> {
         return <Carousel.Item className={"my-3"}>
             <img 
                 className="row p-4 w-50 h-50 mx-auto bg-dark rounded"
                 src={image.src}
-                alt={image.alt}
-                />
+                alt={image.alt}/>
+
             <Card.Title as={"p"} className={"my-1 text-info"}>
-            <code>{image.term}</code><br/><code>{image.alt}</code>
+                <code>{image.term}</code><br/><code>{image.alt}</code>
             </Card.Title>
-    </Carousel.Item>
+
+        </Carousel.Item>
     })   
 }
 
-const JobCardCarousel = ({images}) => (
+const JobCardCarousel = ({imgArr}) => (
     <Card>
         <Carousel   controls={false}
-                fade={true} 
-                interval={5000} 
-                indicators={false} 
-                pauseOnHover={true}
+                    fade={true} 
+                    interval={5000} 
+                    indicators={false} 
+                    pauseOnHover={true}
                 >
-
-        {items(images)}
+        
+        {items(imgArr)}
 
         </Carousel>
     </Card>
