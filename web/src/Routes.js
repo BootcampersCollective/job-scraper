75af4451d3704cbe1c9c5db5256e1c89aa24a4f2
Joshua Smith <jwsmaker@Joshuas-MacBook-Air.local>
2020-02-01 10:47:23 -0700

    modified routes typo

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
-import JobIndex from './screens/JobIndex'
-import RoleShow from './screens/RoleShow'
+import JobIndex from './screens/JobIndex.js'
+import RoleShow from './screens/RoleShow.js'
 
 const Sample = () => (<h1>Sample Functional Component</h1>)
 
